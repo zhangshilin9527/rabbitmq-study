a308fcfc3c2fb508010010d2566b49455e2b1d2e
zhang <zhangsl312@chinaunicom.cn>
2021-01-18 15:19:00 +0800

    修改mq确认消息机制

--- rabbitmq-study-product/src/main/java/com/xiaolinzi/rabbitmq/product/config/MqConfig.java
+++ rabbitmq-study-product/src/main/java/com/xiaolinzi/rabbitmq/product/config/MqConfig.java
@@ -1,7 +1,11 @@
 package com.xiaolinzi.rabbitmq.product.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +18,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableRabbit
 public class MqConfig {
-
+    private static final Logger log = LoggerFactory.getLogger(MqConfig.class);
 
     public static final String DIRECT_EXCHANGE = "xiaolinzi_direct";
     public static final String DIRECT_QUEUE = "xiaolinzi_direct_queue";
@@ -77,6 +81,31 @@ public class MqConfig {
         return BindingBuilder.bind(queue).to(exchange).with(CONFIRM_RETURN_KEY);
     }
 
+    @Bean("asyncRabbitTemplate")
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMandatory(true);
+        //不指定序列化工具
+        //rabbitTemplate.setMessageConverter( new Jackson2JsonMessageConverter());
+        //发送成功确认回调
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            if (ack) {
+                log.info("消息发送成功:correlationData({}),cause({})", correlationData.getId(), cause);
+                log.info("消息发送成功:correlationData({}),cause({})", correlationData.getId(), cause);
+            } else {
+                log.error("消息未成功投递, id:{}, cause:{}", correlationData.getId(), cause);
+            }
+        });
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                log.info("消息内容:{}",new String(message.getBody()));
+                log.info("回复文本:{},回复代码：{}",replyText,replyCode);
+                log.info("交换器名称:{},路由键：{}",exchange,routingKey);
+            }
+        });
+        return rabbitTemplate;
+    }
 
     /**
      *消息一致性配置 end
